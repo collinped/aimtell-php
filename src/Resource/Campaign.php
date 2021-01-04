bb18b74b9c5e9e227645841f931da1cc1402e124
Chris O'Brien <chris@chrisob.com>
2021-01-04 11:35:09 -0800

    add pace to campaign

--- src/Resource/Campaign.php
+++ src/Resource/Campaign.php
@@ -31,4 +31,23 @@ class Campaign extends BaseResource
             $this->resourceName(). '/' .$this->resourceId. '/clicks'
         );
     }
+
+    /**
+     * Get campaign click pace by interval.
+     *
+     * @param int $interval
+     * @return mixed
+     */
+    public function getPace(int $interval = 1000)
+    {
+        $this->confirmResourceIdNotEmpty();
+
+        return $this->sendRequest(
+            'GET',
+            $this->resourceName(). '/' .$this->resourceId. '/pace',
+            [
+                'interval' => $interval,
+            ]
+        );
+    }
 }
